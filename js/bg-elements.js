e8d98b2912071b19a23b6a07c7b1e05907209c01
yantilina <149585144+yantilina@users.noreply.github.com>
2024-10-21 20:35:45 +0500

    Add files via upload

---
+++ js/bg-elements.js
@@ -0,0 +1,8 @@
+const elements = document.querySelectorAll('.set-bg')
+
+console.log(elements);
+for (let i = 0; i < elements.length; i++) {
+    const src = elements[i].dataset.setbg
+
+    elements[i].style.backgroundImage = `url(${src})`
+}
