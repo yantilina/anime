a9ef0e8fec2797328e8c995fb69c926e042f7cfd
badronova.rita@mail.ru <badronova.rita@mail.ru>
2024-10-23 20:49:18 +0500

    anime_2

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,8 +1,11 @@
-const elements = document.querySelectorAll('.set-bg')
+const bgElements = () => {
+    const elements = document.querySelectorAll('.set-bg')
 
-console.log(elements);
-for (let i = 0; i < elements.length; i++) {
-    const src = elements[i].dataset.setbg
+    console.log(elements);
+    for (let i = 0; i < elements.length; i++) {
+        const src = elements[i].dataset.setbg
 
-    elements[i].style.backgroundImage = `url(${src})`
+        elements[i].style.backgroundImage = `url(${src})`
+    }
 }
+bgElements()
